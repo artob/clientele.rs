44990d2281c3df44875fe9a5ead07f0d8f41126e
Arto Bendiken <arto@bendiken.net>
2024-10-15 10:59:13 +0400

    Implement some XDG path getters.

--- lib/clientele/src/paths.rs
+++ lib/clientele/src/paths.rs
@@ -4,7 +4,7 @@ use crate::envs;
 use camino::Utf8PathBuf;
 
 pub fn home() -> Option<Utf8PathBuf> {
-    envs::home().map(Utf8PathBuf::from)
+    envs::home().map(Utf8PathBuf::from) // TODO: Windows
 }
 
 pub fn tmpdir() -> Option<Utf8PathBuf> {
@@ -13,20 +13,40 @@ pub fn tmpdir() -> Option<Utf8PathBuf> {
 
 /// See: https://specifications.freedesktop.org/basedir-spec/latest/#variables
 pub fn xdg_data_home() -> Option<Utf8PathBuf> {
-    envs::xdg_data_home().map(Utf8PathBuf::from)
+    envs::xdg_data_home().map(Utf8PathBuf::from).or_else(|| {
+        home().map(|mut path| {
+            path.push(".local/share");
+            path
+        })
+    })
 }
 
 /// See: https://specifications.freedesktop.org/basedir-spec/latest/#variables
 pub fn xdg_config_home() -> Option<Utf8PathBuf> {
-    envs::xdg_config_home().map(Utf8PathBuf::from)
+    envs::xdg_config_home().map(Utf8PathBuf::from).or_else(|| {
+        home().map(|mut path| {
+            path.push(".config");
+            path
+        })
+    })
 }
 
 /// See: https://specifications.freedesktop.org/basedir-spec/latest/#variables
 pub fn xdg_state_home() -> Option<Utf8PathBuf> {
-    envs::xdg_state_home().map(Utf8PathBuf::from)
+    envs::xdg_state_home().map(Utf8PathBuf::from).or_else(|| {
+        home().map(|mut path| {
+            path.push(".local/state");
+            path
+        })
+    })
 }
 
 /// See: https://specifications.freedesktop.org/basedir-spec/latest/#variables
 pub fn xdg_cache_home() -> Option<Utf8PathBuf> {
-    envs::xdg_cache_home().map(Utf8PathBuf::from)
+    envs::xdg_cache_home().map(Utf8PathBuf::from).or_else(|| {
+        home().map(|mut path| {
+            path.push(".cache");
+            path
+        })
+    })
 }
